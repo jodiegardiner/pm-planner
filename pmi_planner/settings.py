75779c96211426ca29624150b2c8d8d1bae19c6e
Jodie Gardiner <jodiegardiner@outlook.com>
2016-11-25 16:56:58 +0000

    corrected Procfile

--- pmi_planner/settings.py
+++ pmi_planner/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pm-planner.herokuapp.com/']
+ALLOWED_HOSTS = ['pm-planner.herokuapp.com']
 
 
 # Application definition
