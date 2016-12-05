dd8fe504ceab460a979b0a51bef7a6c7e9e86195
Jodie Gardiner <jodiegardiner@outlook.com>
2016-12-05 13:38:30 +0000

    forum models changed to timezone.now

--- threads/models.py
+++ threads/models.py
@@ -18,7 +18,7 @@ class Thread(models.Model):
     name = models.CharField(max_length=255)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='threads')
     subject = models.ForeignKey(Subject, related_name='threads')
-    created_at = models.DateTimeField(default=timezone.now())
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __unicode__(self):
         return self.name
@@ -28,5 +28,5 @@ class Post(models.Model):
     thread = models.ForeignKey(Thread, related_name='posts')
     comment = HTMLField(blank=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts')
-    created_at = models.DateTimeField(default=timezone.now())
+    created_at = models.DateTimeField(default=timezone.now)
 
