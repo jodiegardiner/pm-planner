4bb44d1d2d72eab942f8fffcc44b7fb01b478202
Jodie Gardiner <jodiegardiner@outlook.com>
2016-12-05 15:26:58 +0000

    edit settings to fix AWS upload issue

--- pmi_planner/settings.py
+++ pmi_planner/settings.py
@@ -161,6 +161,7 @@ AWS_S3_HOST = 's3-eu-west-1.amazonaws.com'
 # This is used by the `static` template tag from `static`, if you're using that. Or if anything else
 # refers directly to STATIC_URL. So it's safest to always set it.
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_LOCATION = 'static'
 
 # Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
 # you run `collectstatic`).
